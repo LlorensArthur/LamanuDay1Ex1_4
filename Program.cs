0a7b7f3460cd472d537182d4d35b5f6c7ab78df6
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:02:25 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿string Civility = "Monsieur";
+string Name = "Vincent";
+int visitNumber = 1;
+Console.WriteLine($"Bonjour {Civility} {Name}, vous êtes venu nous rendre visite {visitNumber} fois");
